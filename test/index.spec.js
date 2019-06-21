b5fe73f22e86036e7c42c7bd73ca2aff43f47038
georgeben <msdcconnect@gmail.com>
2019-06-21 17:12:31 +0100

    Add tests

---
+++ test/index.spec.js
@@ -0,0 +1,23 @@
+const { getUniversities } = require('../index');
+const fs = require('fs')
+const expect = require('chai').expect
+const path = require('path')
+
+describe('index.js', function(){
+    it('smoke tests', function(){
+        expect(1).to.equal(1)
+    })
+
+    it('should return list of all universities', function(){
+        let universities = JSON.parse(fs.readFileSync(path.join(__dirname, '../db/universities.json'), 'utf-8'))
+        universities = JSON.stringify(universities)
+        expect(getUniversities()).to.deep.equal(universities)
+    })
+
+    it('should throw an error if a wrong parameter for category is passed', function(){
+        expect(function(){
+            getUniversities(1)
+        }).to.throw('IllegalArgumentException - Category must be a string')
+    })
+})
+
